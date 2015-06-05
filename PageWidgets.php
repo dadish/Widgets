942d986b9118fa651e9cf4c5f81090359e9aef9b
Nurguly Ashyrov <dadish.na@gmail.com>
2015-06-05 07:51:57 +0500

    Filter widgets by id.

--- PageWidgets.php
+++ PageWidgets.php
@@ -2,4 +2,11 @@
 
 class PageWidgets extends TemplateWidgets {
 
+  public function refresh()
+  {
+    $this->removeAll();
+    $this->import($this->widgets->find("owner=$this->ownerId, sort=sort"));
+    $this->breakpoints->fetchAllForOwner($this->ownerId);
+  }
+
 }
